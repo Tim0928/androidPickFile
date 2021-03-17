220431493869df1b434972c058df7ba2f9bd73fb
timchou <tim_chou@kusauto.com.tw>
2021-03-17 09:55:39 +0800

    first commit

---
+++ app/src/main/java/com/example/simplepickfile/MainActivity.java
@@ -0,0 +1,299 @@
+package com.example.simplepickfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextPath;
+    private static final int MY_REQUEST_CODE_PERMISSION = 1000;
+    private static final int MY_REQUEST_CODE_WRITE_PERMISSION = 1001;
+    private static final int MY_RESULT_CODE_FILECHOOSER = 2000;
+    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextPath = (EditText) findViewById(R.id.editText_path);
+        askWritePermission();
+    }
+
+    public void pickonClick(View view){
+        askPermissionAndBrowseFile();
+    }
+
+
+
+
+    private void askWritePermission()  {
+        // With Android Level >= 23, you have to ask the user
+        // for permission to access External Storage.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // Level 23
+
+
+            int permisson = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (permisson != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_REQUEST_CODE_WRITE_PERMISSION);
+                return;
+            }
+        }
+    }
+
+    private void askPermissionAndBrowseFile()  {
+        // With Android Level >= 23, you have to ask the user
+        // for permission to access External Storage.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // Level 23
+
+            // Check if we have Call permission  取得權限
+            int permisson = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permisson != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_REQUEST_CODE_PERMISSION);
+                return;
+            }
+        }
+        doBrowseFile();
+    }
+
+    //open folder
+    private void doBrowseFile(){
+        Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFileIntent.setType("*/*");
+        // Only return URIs that can be opened with ContentResolver
+        chooseFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose a file");
+        startActivityForResult(chooseFileIntent, MY_RESULT_CODE_FILECHOOSER);
+    }
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_REQUEST_CODE_PERMISSION:{
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (CALL_PHONE).
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG,"Permission granted!");
+                    Toast.makeText(getApplicationContext(), "Permission granted!", Toast.LENGTH_SHORT).show();
+
+                    doBrowseFile();
+                }else {
+                    Log.i(TAG,"Permission denied!");
+                    Toast.makeText(getApplicationContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+
+            case MY_REQUEST_CODE_WRITE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG,"Permission granted!");
+                    Toast.makeText(getApplicationContext(), "Permission granted!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Log.i(TAG,"Permission denied!");
+                    Toast.makeText(getApplicationContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+private File file;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case MY_RESULT_CODE_FILECHOOSER:
+                if (resultCode == Activity.RESULT_OK ) {
+                    if(data != null){
+                        Uri fileUri = data.getData();
+                        Log.i(TAG, "Uri: " + fileUri);
+                        String filePath = null;
+                        try {
+                            filePath = FileUtils.getPath(getApplicationContext(),fileUri);
+                        } catch (Exception e) {
+                            Log.e(TAG,"Error: " + e);
+                            Toast.makeText(getApplicationContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                        }
+                        editTextPath.setText(filePath);
+//                        filePath
+//                        String myUrl = "http://stackoverflow.com";
+                        try {
+
+//                            File file = new File(fileUri.getPath());//create path from uri
+                            MetaData metaData = dumpImageMetaData(fileUri);
+                            String str = readTextFromUri(fileUri);
+                            Log.i(TAG, "onActivityResult: "+str);
+
+                            byte[] bytes = str.getBytes();
+//                            writeBinaryFile(bytes, Environment.getExternalStorageDirectory().getPath()+File.separator+"good.bin");
+                            String STR = filePath.replace(metaData.getDisplayName(),"");
+                            writeBinaryFile(bytes, STR+"good.bin");
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                        //-------------------------------
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void writeBinaryFile(byte[] aBytes, String aFileName) throws IOException {
+        Path path = Paths.get(aFileName);
+        Files.write(path, aBytes); //creates, overwrites
+
+
+//        InputStream fis = getContentResolver().openInputStream(Uri.parse(aFileName));
+//        fis.read(aBytes);
+    }
+
+//    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+//        Cursor cursor = null;
+////        String[] projection = { MediaStore.Images.Media.DATA };
+//        String[] projection = {  };
+//        try {
+//            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+//            if (cursor != null && cursor.moveToFirst()) {
+//                int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//                return cursor.getString(index);
+//            }
+//        } finally {
+//            if (cursor != null)
+//                cursor.close();
+//        }
+//        return null;
+//    }
+//
+//
+//    public static String getMimeType(String url) {
+//        String type = null;
+//        String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+//        if (extension != null) {
+//            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+//        }
+//        return type;
+//    }
+
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
+    public MetaData dumpImageMetaData(Uri uri) {
+        String replacestr = null;
+        String token = uri.getLastPathSegment();//getPath();
+        Log.i(TAG, "path: " + token);
+        MetaData metaData = new MetaData();
+        // The query, because it only applies to a single document, returns only
+        // one row. There's no need to filter, sort, or select fields,
+        // because we want all fields for one document.
+        Cursor cursor = getApplicationContext().getContentResolver()
+                .query(uri, null, null, null, null, null);
+
+        try {
+            // moveToFirst() returns false if the cursor has 0 rows. Very handy for
+            // "if there's anything to look at, look at it" conditionals.
+            if (cursor != null && cursor.moveToFirst()) {
+
+                // Note it's called "Display Name". This is
+                // provider-specific, and might not necessarily be the file name.
+                String displayName = cursor.getString(
+                        cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                Log.i(TAG, "Display Name: " + displayName);
+//                token-displayName;
+
+                int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                // If the size is unknown, the value stored is null. But because an
+                // int can't be null, the behavior is implementation-specific,
+                // and unpredictable. So as
+                // a rule, check if it's null before assigning to an int. This will
+                // happen often: The storage API allows for remote files, whose
+                // size might not be locally known.
+                metaData.setDisplayName(displayName);
+                String size = null;
+                if (!cursor.isNull(sizeIndex)) {
+                    // Technically the column stores an int, but cursor.getString()
+                    // will do the conversion automatically.
+                    size = cursor.getString(sizeIndex);
+                    metaData.setSize(size);
+                } else {
+                    size = "Unknown";
+                }
+                Log.i(TAG, "Size: " + size);
+            }
+        } finally {
+            cursor.close();
+            return metaData;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/simplepickfile/MetaData.java
@@ -0,0 +1,22 @@
+package com.example.simplepickfile;
+
+public class MetaData {
+    private String displayName;
+    private String size;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
